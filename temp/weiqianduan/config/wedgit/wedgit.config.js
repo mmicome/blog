01756a1bc443d293ce5489140262c018b4448fde
一介书生 <813221186@qq.com>
2020-10-13 21:27:40 +0800

    Update wedgit.config.js

--- temp/weiqianduan/config/wedgit/wedgit.config.js
+++ temp/weiqianduan/config/wedgit/wedgit.config.js
@@ -3,13 +3,22 @@ var path = require('path');
 module.exports = {
     prefix: '', // 可以指定特定的前缀，防止注册的组件名称发生冲突
     paths: [ // 指定需要发布的组件路径
-        '../src/components/remotetest.vue',
+        // '../src/components/remotetest.vue',
+        '../src/main.js',
+        // '../src/App.vue',
+        // '../src/views/Home.vue',
+        // '../src/views/About.vue',
+        // '../src/views/Self.vue'
         // '../apps/hello/a.vue'
     ],
-    out: '../public/static/apps/', // 指定放置发布文件(.js)的目录，即远程访问的目录
-    js: 'static/apps/',
-    img: 'resources/imgs/',
+    out: '../public/apps/', // 指定放置发布文件(.js)的目录，即远程访问的目录
+    js: 'js/',
+    css: 'css/',
+    img: 'imgs/',
+    remote: 'apps/',
+    npm: 'chunk/npm/',
+    domain: 'http://10.188.196.156:3000/',
     alias: { // 如果组件中使用了alias，需要在此处指定
-        // 'bh-vue': path.resolve(__dirname, '../node_modules/bh-vue')
+        '@': path.resolve(__dirname, '../src')
     }
 };
