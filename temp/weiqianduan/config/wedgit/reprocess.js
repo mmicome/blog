84d871ccb3f29fe58c353ec924248cadacda5cf7
一介书生 <813221186@qq.com>
2020-10-13 21:26:33 +0800

    Update reprocess.js

--- temp/weiqianduan/config/wedgit/reprocess.js
+++ temp/weiqianduan/config/wedgit/reprocess.js
@@ -4,36 +4,32 @@ var basename = require('path').basename;
 
 var widgetsConf = require('./widgets.conf.js');
 var widgets = widgetsConf.paths;
-var out = widgetsConf.out;
-var js = widgetsConf.js;
-var localePath = resolve(__dirname, out);
-var fileList = fs.readdirSync(localePath);
+var out = resolve(__dirname, widgetsConf.out);
+var npm = resolve(__dirname, widgetsConf.out + widgetsConf.npm);
+var css = resolve(__dirname, widgetsConf.out + widgetsConf.css);
+var js = widgetsConf.remote;
+var fileList = fs.readdirSync(out);
+var npmList = fs.readdirSync(npm);
+var cssList = fs.readdirSync(css);
 var includes = [];
 
 widgets.forEach(item => {
-  if (item) {
-    var m = item.match(/(\w+)\.vue$/);
-    if (!m || m.length !== 2) {
-      console.log('widget 路径定义格式不正确，请以.vue结尾！');
-    } else {
-      var entryName = m[1] + '.js';
-      includes.push(entryName);
-    }
-  } else {
-    console.error('widget 路径定义不可为空！');
-  }
+  var m = item.match(/(\w+)\.(vue|js)$/);
+  var entryName = m[1] + '.js';
+  includes.push(entryName);
 });
 fileList.filter(function(file) {
   return includes.indexOf(file) > -1;
 }).forEach(function(file) {
-  let filepath = resolve(localePath, file);
+  let filepath = resolve(out, file);
   var name = basename(file, '.js');
   fs.readFile(filepath, {encoding: "utf-8"}, function(err, fr) {
     if (err) {
       console.log(err);
      }else {
-      var buffer = fr.replace(/{/, "{__webpack_require__.scope=window['" + name +"_domainscope'];")
-      .replace(/return __webpack_require__\.p/, "return __webpack_require__.scope + '" + js + "' + __webpack_require__.p");
+      var buffer = fr.replace(/{/, "{__webpack_require__.scope='" + widgetsConf.domain +"';")
+      .replace(/return __webpack_require__\.p/, "return __webpack_require__.scope + '" + js + "' + __webpack_require__.p")
+      .replace(/fullhref = __webpack_require__.p/, "fullhref =  __webpack_require__.scope + '" + js + "' + __webpack_require__.p");//
       fs.writeFile(filepath, buffer, 'utf8', (err) => {
           if (err) throw err;
           console.log('success done');
@@ -41,3 +37,16 @@ fileList.filter(function(file) {
      }
   })
 })
+let common = []
+npmList.forEach(function(file) {
+  // if (cssList.indexOf)
+  // var m = (file).match(/(\w+)\.(css|js)$/);
+  common.push(widgetsConf.domain + widgetsConf.remote + widgetsConf.npm + file)
+})
+cssList.forEach(function(file) {
+  // if (cssList.indexOf)
+  // var m = (file).match(/(\w+)\.(css|js)$/);
+  common.push(widgetsConf.domain + widgetsConf.remote + widgetsConf.css + file)
+})
+common.push(widgetsConf.domain + widgetsConf.remote + 'chunk/styles.b13981.js')
+fs.writeFileSync(resolve(out, 'common.json'), JSON.stringify(common), 'utf-8');
